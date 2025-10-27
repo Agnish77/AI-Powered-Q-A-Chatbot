e4adc91dd4d828b2199d9ba91aa2fd42aa962e1e
Agnish Paul <97803357+Agnish77@users.noreply.github.com>
2025-10-27 14:20:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from dotenv import load_dotenv
 
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.chains.combine_documents.chain import create_stuff_documents_chain
-
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
