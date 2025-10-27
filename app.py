a3b9cfa0bd9cbb2dbde5f0aa271f77ff478a4d58
Agnish Paul <97803357+Agnish77@users.noreply.github.com>
2025-10-27 14:48:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # Load environment variables
